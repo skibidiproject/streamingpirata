304fabb31a61b0c48c2dffdcdf4370b33f2d792d
skibidiproject <daddyalberty@gmail.com>
2025-08-27 21:52:36 +0200

    fix

--- src/app/api/contents/[type]/[id]/episode/[season]/[episode]/route.ts
+++ src/app/api/contents/[type]/[id]/episode/[season]/[episode]/route.ts
@@ -3,20 +3,30 @@ import pool from "@/app/lib/database";
 
 export async function GET(
     request: Request,
-    { params }: { params: { id: string, season: number, type: string, episode: number } }) {
+    { params }: { params: Record<string, string> }) { // Modifica qui
     const type = params.type;
     if (type === "tv") {
         const id = params.id;
-        const season = params.season;
-        const episode = params.episode
-        const result = await pool.query(`WITH episode AS (
-SELECT ts.media_id, ts.season_number, te.* FROM tv_episodes te INNER JOIN tv_seasons ts ON ts.id = te.season_id
-	WHERE media_id = $1 
-		AND episode_number = $2
-		AND season_number = $3 ) 
-SELECT m.title AS media_title, e.* FROM episode e INNER JOIN media m ON e.media_id = m.id;`, [id, episode, season]);
+        const season = parseInt(params.season, 10); // Assicurati che season sia un numero
+        const episode = parseInt(params.episode, 10); // Assicurati che episode sia un numero
+        const result = await pool.query(`
+            WITH episode AS (
+                SELECT ts.media_id, ts.season_number, te.* 
+                FROM tv_episodes te 
+                INNER JOIN tv_seasons ts ON ts.id = te.season_id
+                WHERE media_id = $1 
+                    AND episode_number = $2 
+                    AND season_number = $3
+            )
+            SELECT m.title AS media_title, e.* 
+            FROM episode e 
+            INNER JOIN media m ON e.media_id = m.id;
+        `, [id, episode, season]);
+        
         const media = result.rows[0];
-
         return NextResponse.json(media);
     }
+
+    // Aggiungi una gestione del caso in cui 'type' non è 'tv'
+    return NextResponse.json({ error: "Invalid type" }, { status: 400 });
 }
