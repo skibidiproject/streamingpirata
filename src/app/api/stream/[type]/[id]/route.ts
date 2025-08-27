9079b57e7b3d3acee7ecb4c4f4f0f9fb65b1bf43
skibidiproject <daddyalberty@gmail.com>
2025-08-27 20:34:26 +0200

    fix cors

--- src/app/api/stream/[type]/[id]/route.ts
+++ src/app/api/stream/[type]/[id]/route.ts
@@ -27,7 +27,12 @@ export async function GET(req: NextRequest, { params }: { params: { type: string
       host: "VixCloud",
       redirect_stream: "true",
     },
-    api_password: process.env.MEDIAFLOW_PASSWORD,
+    api_password: process.env,
+    response_headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    }
   };
 
   const proxyResponse = await fetch(`${process.env.MEDIAFLOW_BASE_URL}/generate_url`, {
